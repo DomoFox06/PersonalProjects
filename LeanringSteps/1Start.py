d41d900ed1ed56f97f93a8c0dd69128f6a609d87
DomoFox <yarnalld0401@student.faytechcc.edu>
2026-05-19 01:39:38 +0000

    pythonSave

---
+++ LeanringSteps/1Start.py
@@ -0,0 +1,10 @@
+# Learning to Print and use Numbers
+
+
+
+print ("Hello World!")
+
+print ("Did you know 2+3 is: " , 2+3)
+
+name = input("Do tell, what's your name? ")
+print (f"Hello there, {name}!")
